6bc89d83b85839205661aea41ae45c0141e5b4aa
moacirdavidag <moacirdavidag@gmail.com>
2022-08-19 13:51:41 -0300

    Atributos obrigatórios da entidade Palestrante modificados

--- model/palestrante.js
+++ model/palestrante.js
@@ -1,15 +1,16 @@
 const mongoDB = require('../database/mongo');
 
+
 const schema = new mongoDB.Schema({
     nome: {type: String, required: true},
     sobrenome: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     senha: {type: String, required: true},
-    minicurriculo: {type: String, required: true},
+    minicurriculo: {type: String},
     status: {type: String, required: true},
-    site: {type: String, required: true},
-    curriculoLattes: {type: String, required: true},
-    redeSocial: {type: String, required: true}
+    site: {type: String},
+    curriculoLattes: {type: String},
+    redeSocial: {type: String}
 })
 
 const Palestrante = mongoDB.model('Palestrante', schema);
