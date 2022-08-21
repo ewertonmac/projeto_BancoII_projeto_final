32af62dbb82e19455a6b0aa891e948d2bfc8b8fd
Ewerton Maciel <ewertoncz@gmail.com>
2022-08-21 15:31:40 -0300

    ajuste config conexão mongoAtlas

--- database/mongo.js
+++ database/mongo.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const mongoHost = process.env.MONGO_HOST || 'localhost';
+const mongoHost = process.env.MONGO_HOST;
 const mongoPort = process.env.MONGO_PORT || '27017';
-const mongoUser = process.env.MONGO_USER || '';
-const mongoPass = process.env.MONGO_PASS || '';
+const mongoUser = process.env.MONGO_USER;
+const mongoPass = process.env.MONGO_PASS;
 const mongoDB = process.env.MONGO_DB || 'even4';
 
-const atlasURI = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:${mongoPort}/${mongoDB}`;
-const localURI = `mongodb://${mongoHost}:${mongoPort}/${mongoDB}`;
+const atlasURI = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoHost}/${mongoDB}?retryWrites=true&w=majority`;
+const localURI = `mongodb://localhost:${mongoPort}/${mongoDB}`;
 
 const connectionURI = mongoUser && mongoPass ? atlasURI : localURI;
 
-mongoose.connect(connectionURI, { useNewUrlParser: true });
+try {
+    mongoose.connect(connectionURI, { useNewUrlParser: true , useUnifiedTopology: true });
+
+} catch (error) {
+    console.log(error.message);
+}
 
 module.exports = mongoose;
