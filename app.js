2ff1ebc41a135267731a2b50cddf0e1d5b97b354
Ewerton Maciel <ewertoncz@gmail.com>
2022-08-24 13:12:32 -0300

    ajuste configurações redis

--- app.js
+++ app.js
@@ -79,5 +79,5 @@ app.use((req, res) => {
 
 // server
 
-app.listen(port, () => console.log(`O servidor está rodando em http://localhost:${port}`));
+app.listen(port, () => console.log(`O servidor está rodando na porta: ${port}`));
 
--- database/redis.js
+++ database/redis.js
@@ -2,13 +2,26 @@ const redis = require('redis');
 let RedisStore = require("connect-redis");
 require('dotenv').config();
 
+const username = process.env.REDIS_USER
+const password = process.env.REDIS_PASSWORD
+const host = process.env.REDIS_HOST || 'localhost'
+const port = process.env.REDIS_PORT || '6379'
+
+const redisLabs = `redis://${username}:${password}@${host}:${port}`
+const local = `redis://${host}:${port}`
+
+const connectionURI = username && password ? redisLabs : local;
+
 const redisClient = redis.createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    url: connectionURI,
     legacyMode: true
 });
 
-redisClient.connect().catch(console.error)
+try{
+    redisClient.connect().catch(console.error)
+
+}catch(err){
+    console.log(err)
+}
 
 module.exports = redisClient;
-
-
