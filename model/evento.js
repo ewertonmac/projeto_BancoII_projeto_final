1e00efbfc36c029c67d71dfe041568aab21101e7
Fabiola Diniz <fabioladiniz36@hotmail.com>
2022-08-30 15:32:10 +0000

    atualizações

--- model/evento.js
+++ model/evento.js
@@ -2,31 +2,85 @@ const mongoDB = require('../database/mongo');
 const Usuario = require('./usuario');
 
 const schema = new mongoDB.Schema({
-    nome: { type: String, required: true, unique: true },
-    descricao: { type: String, required: true },
-    data: { type: Date, required: true },
-    urlImagem: { type: String, require: true },
-    siteOficial: { type: String, required: true },
+    nome: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        required: true
+    },
+    urlImagem: {
+        type: String,
+        require: true
+    },
+    siteOficial: {
+        type: String,
+        required: true
+    },
     categoria: String,
     palestrante: {
         type: {
-            nome: { type: String, required: true },
-            sobrenome: { type: String, required: true },
-            _id: { type: String, required: true},
-            email: { type: String, required: true },
-            minicurriculo: { type: String, required: true},
-            curriculoLattes: { type: String, required: true},
-            site: { type: String, required: true},
-            twitterUser: { type: String, required: false},
-            instagramUser: { type: String, required: false},
-        }, required: true
+            nome: {
+                type: String,
+                required: true
+            },
+            sobrenome: {
+                type: String,
+                required: true
+            },
+            _id: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true
+            },
+            minicurriculo: {
+                type: String,
+                required: true
+            },
+            curriculoLattes: {
+                type: String,
+                required: true
+            },
+            site: {
+                type: String,
+                required: true
+            },
+            twitterUser: {
+                type: String,
+                required: false
+            },
+            instagramUser: {
+                type: String,
+                required: false
+            },
+        },
+        required: true
     },
     ouvintes: {
         type: [{
-            nome: { type: String, required: true },
-            sobrenome: { type: String, required: true },
-            email: { type: String, required: true},
-        }], required: false
+            nome: {
+                type: String,
+                required: true
+            },
+            sobrenome: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true
+            },
+        }],
+        required: false
     },
 });
 
