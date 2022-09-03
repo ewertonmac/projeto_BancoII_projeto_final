3a85b8f645078b133d59db448cb609427d27e8e0
Ewerton <ewerton.maciel@cwi.com.br>
2022-09-03 16:34:57 -0300

    finalizando testes unitarios

--- service/usuarioService.spec.js
+++ service/usuarioService.spec.js
@@ -10,182 +10,167 @@ jest.mock("../repository/usuarioRepository", () => ({
     deletar: jest.fn()
 }))
 
+let result
+let exceptionMessage
+
+let usuarioValido = {
+    nome: "Ewerton",
+    sobrenome: "Maciel",
+    email: "ewerton@mail.com",
+    senha: "minhasenha",
+    status: "estudante"
+}
+
+let usuarioInvalido = {
+    nome: "Ewerton",
+    sobrenome: "Maciel",
+    email: "ewerton@mail.com",
+    senha: "minhasenha",
+    status: ""
+}
+
+let usuarioRetorno = {
+    nome: "ewerton",
+    sobrenome: "maciel"
+}
+
+let  usuarioAtualizarValido = {
+    ...usuarioValido,
+    minicurriculo: "Discente do curso de ADS IFPB",
+    urlImagemPerfil: "https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg?w=2000",
+    instituicao: "IFPB",
+    telefone: "1234567890",
+    site: "www.meusite.com",
+    githubUser: "@ewertonmac",
+    twitterUser: "@ewertonmac",
+    instagramUser: "@ewertonmac",
+    curriculoLattes: "www.lattes.cnpq.br"
+}
+
+let usuarioAtualizarInvalido = {
+    ...usuarioInvalido
+}
+
+const idValido = "630fd127557cc786c3d4abe5"
+
+beforeEach(() => {
+    result = null
+    exceptionMessage = null
+})
+
 describe("Listar por id", () => {
     test("Deve listar usuário quando id informado", async () => {
-
+        repository.listarPorId.mockReturnValue([usuarioRetorno])
         try {
-
-            const usuarioRetorno = {
-                nome: "ewerton",
-                sobrenome: "maciel"
-            }
-
-            repository.listarPorId.mockReturnValue([usuarioRetorno])
-            const result = await usuarioService.listarPorId(1)
-
-            expect(result.length).toBe(1)
-
+            result = await usuarioService.listarPorId(1)
         } catch (error) { }
 
+        expect(result.length).toBe(1)
+        expect(result[0]).toEqual(usuarioRetorno)
     })
 
     test("Não deve listar usuário quando id informado inválido", async () => {
-
         try {
-            await usuarioService.listarPorId(null)
-
+            result = await usuarioService.listarPorId(null)
         } catch (error) {
-            expect(error.message).toEqual("id de usuário não informado")
+            exceptionMessage = error.message
         }
+
+        expect(result).toBeNull()
+        expect(exceptionMessage).toEqual("id de usuário não informado")
 
     })
 })
 
 describe("Listar por email", () => {
     test("Deve listar usuário quando email informado", async () => {
-
+        repository.listarPorEmail.mockReturnValue([usuarioRetorno])
         try {
-
-            const usuarioRetorno = {
-                nome: "ewerton",
-                sobrenome: "maciel"
-            }
-
-            repository.listarPorEmail.mockReturnValue([usuarioRetorno])
-            const result = await usuarioService.listarPorEmail("meu@email.com")
-
-            expect(result.length).toBe(1)
-
+            result = await usuarioService.listarPorEmail("meu@email.com")
         } catch (error) { }
 
+        expect(result.length).toBe(1)
+        expect(result[0]).toEqual(usuarioRetorno)
     })
 
     test("Não deve listar usuário quando id informado inválido", async () => {
-
         try {
-            await usuarioService.listarPorEmail(null)
-
+            result = await usuarioService.listarPorEmail(null)
         } catch (error) {
-            expect(error.message).toEqual("email não informado")
+            exceptionMessage = error.message
         }
 
+        expect(exceptionMessage).toEqual("email não informado")
+        expect(result).toBeNull()
     })
 })
 
 describe("Cadastrar usuario", () => {
 
     test("Deve cadastrar usuário válido", async () => {
-        const usuarioValido = {
-            nome: "Ewerton",
-            sobrenome: "Maciel",
-            email: "ewerton@mail.com",
-            senha: "minhasenha",
-            status: "estudante"
-        }
-
+        repository.cadastrar.mockReturnValue(true)
 
         try {
-            repository.cadastrar.mockReturnValue(true)
-            const result = await usuarioService.cadastrar(usuarioValido)
-            expect(result).toBeTruthy()
+            result = await usuarioService.cadastrar(usuarioValido)
         } catch (error) { }
 
+        expect(result).toBeTruthy()
     })
 
     test("Não deve cadastrar usuario inválido", async () => {
-        const usuarioInvalido = {
-            nome: "Ewerton",
-            sobrenome: "Maciel",
-            email: "ewerton@mail.com",
-            senha: "minhasenha",
-            status: ""
-        }
 
         try {
-            const result = await usuarioService.cadastrar(usuarioInvalido)
-            expect(result).toBeNull()
+            result = await usuarioService.cadastrar(usuarioInvalido)
         } catch (error) {
-            expect(error.message).toEqual("todos os dados devem ser informados")
+            exceptionMessage = error.message
         }
+        expect(result).toBeNull()
+        expect(exceptionMessage).toEqual("todos os dados devem ser informados")
     })
 })
 
 describe("Atualizar usuário", () => {
     test("Deve atualizar um usuário válido", async () => {
 
+        repository.atualizar.mockReturnValue(true)
         try {
-            const usuarioAtualizarValido = {
-                nome: "Ewerton",
-                sobrenome: "Maciel",
-                email: "ewertoncz@gmail.com",
-                minicurriculo: "Discente do curso de ADS IFPB",
-                urlImagemPerfil: "https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg?w=2000",
-                instituicao: "IFPB",
-                telefone: "1234567890",
-                site: "www.meusite.com",
-                githubUser: "@ewertonmac",
-                twitterUser: "@ewertonmac",
-                instagramUser: "@ewertonmac",
-                curriculoLattes: "www.lattes.cnpq.br",
-            }
-
-            const idValido = "630fd127557cc786c3d4abe5"
-
-            repository.atualizar.mockReturnValue(true)
-            const result = await usuarioService.atualizar(idValido, usuarioAtualizarValido)
-
-            expect(result).toBeTruthy()
+            result = await usuarioService.atualizar(idValido, usuarioAtualizarValido)
         } catch (error) { }
+
+        expect(result).toBeTruthy()
     })
 
     test("Não deve atualizar usuário com dados inválidos", async () => {
-
         try {
-
-            const usuarioAtualizarInvalido = {
-                nome: "Ewerton",
-                sobrenome: "Maciel",
-                email: "ewertoncz@gmail.com",
-                minicurriculo: "Discente do curso de ADS IFPB",
-                urlImagemPerfil: "",
-                instituicao: "IFPB",
-                telefone: "",
-                site: "www.meusite.com",
-                githubUser: "@ewertonmac",
-                twitterUser: "@ewertonmac",
-                instagramUser: "@ewertonmac",
-                curriculoLattes: "www.lattes.cnpq.br",
-            }
-
-            const idValido = "630fd127557cc786c3d4abe5"
-
-            const result = await usuarioService.atualizar(idValido, usuarioAtualizarInvalido)
-
+            result = await usuarioService.atualizar(idValido, usuarioAtualizarInvalido)
         } catch (error) {
-            expect(error.message).toEqual("todos os dados devem ser informados")
+            exceptionMessage = error.message
         }
+        expect(result).toBeNull()
+        expect(exceptionMessage).toEqual("todos os dados devem ser informados")
     })
 })
 
 describe("Deletar usuário", () => {
     test("Deve deletar quando id válido for informado", async () => {
 
+        repository.deletar.mockReturnValue(true)
         try {
-            const idValido = "630fd127557cc786c3d4abe5"
-
-            repository.deletar.mockReturnValue(true)
-            const result = await usuarioService.deletar(idValido)
-
-            expect(result).toBeTruthy()
+            result = await usuarioService.deletar(idValido)
         } catch (error) { }
+
+        expect(result).toBeTruthy()
     })
 
     test("Não deve deletar quando id inválido", async () => {
 
         try {
-            await usuarioService.deletar("")
+            result = await usuarioService.deletar("")
         } catch (error) {
-            expect(error.message).toEqual("id não informado")
+            exceptionMessage = error.message
         }
+
+        expect(result).toBeNull()
+        expect(exceptionMessage).toEqual("id não informado")
     })
 })
